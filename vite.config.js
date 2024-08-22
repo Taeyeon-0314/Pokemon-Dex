c51c25ba6f9364f76a163aa377a5e122269a5a68
Taeyeon <lty0314@gmail.com>
2024-08-22 21:24:53 +0900

    배포테스트중

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Pokemon-Dex', // 여기에는 GitHub Pages에서 사용되는 저장소 이름을 넣어줘
+  base: '/Pokemon-Dex/', // 여기에는 GitHub Pages에서 사용되는 저장소 이름을 넣어줘
 
 })
