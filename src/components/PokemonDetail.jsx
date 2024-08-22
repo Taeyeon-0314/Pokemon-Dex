7343081079cfb3f51040fbbf1504d75dca3f0cc5
Taeyeon <lty0314@gmail.com>
2024-08-22 20:20:46 +0900

    feat: PokemonDetail 컴포넌트 추가 , 리스트에서 선택한 포켓몬의 상세정보를 확인할 수 있음

---
+++ src/components/PokemonDetail.jsx
@@ -0,0 +1,89 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import MOCK_DATA from "../mock";
+import GlobalStyle from "../css/GlobalStyle";
+import styled from "styled-components";
+
+const StDetailImg = styled.img`
+  width: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StDetailH2 = styled.h2`
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+  color: red;
+`;
+
+const StDetailTypeP = styled.p`
+  font-size: 20px;
+  text-align: center;
+  margin-top: 20px;
+`;
+
+const StDetailDescriptionP = styled.p`
+  text-align: center;
+  margin-top: 20px;
+`;
+
+const StDetailBackBtn = styled.button`
+  background-color: black;
+  width: 200px;
+  height: 50px;
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  margin-top: 20px;
+`;
+const StDetailBackDiv = styled.div`
+  text-align: -webkit-center;
+`;
+function PokemonDetail() {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const pokemonId = Number(queryParams.get("id")); // 포켓몬 ID 를 쿼리스트링으로부터 받아옵시다.
+  //   console.log(pokemonId);
+
+  const pokemon = MOCK_DATA.find((p) => p.id === pokemonId);
+  //   console.log(pokemon);
+
+  if (!pokemon) {
+    return <div>포켓몬을 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <StDetailBackDiv>
+          <StDetailImg src={pokemon.img_url} alt={pokemon.korean_name} />
+        </StDetailBackDiv>
+        <StDetailH2>{pokemon.korean_name}</StDetailH2>
+        <StDetailTypeP>타입: {pokemon.types.join(", ")}</StDetailTypeP>
+        <StDetailDescriptionP>{pokemon.description}</StDetailDescriptionP>
+        {/* onClick 이벤트를 완성해주세요. */}
+        <StDetailBackDiv>
+          <StDetailBackBtn
+            onClick={() => {
+              navigate(`/dex`);
+            }}
+          >
+            뒤로 가기
+          </StDetailBackBtn>
+        </StDetailBackDiv>
+      </div>
+    </>
+  );
+}
+
+export default PokemonDetail;
