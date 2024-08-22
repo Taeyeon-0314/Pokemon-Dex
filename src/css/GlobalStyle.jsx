d91bf4e3e01f4620368a3f19b1ff86c7b00b37ff
Taeyeon <lty0314@gmail.com>
2024-08-22 11:27:10 +0900

    feat: 홈페이지 구현

---
+++ src/css/GlobalStyle.jsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body{
+background-color: #c7b199;
+width: 100%;
+height: 100vh;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;    
+}
+
+`;
+
+export default GlobalStyle;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import GlobalStyle from "../css/GlobalStyle";
+
+const StDevBtn = styled.button`
+  background-color: #fa4747;
+  width: 200px;
+  height: 50px;
+  font-size: 20px;
+  font-family: bold;
+  color: white;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StImg = styled.img`
+  width: 700px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StMainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <GlobalStyle />
+      <StMainDiv>
+        <StImg src="/mainPokemon.png" alt="" />
+        <StDevBtn onClick={() => navigate("/dex")}>
+          포켓몬 도감 시작하기
+        </StDevBtn>
+      </StMainDiv>
+    </>
+  );
+}
+
+export default Home;
