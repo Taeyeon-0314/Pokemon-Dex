8f2c6356d24741dd0aa356ccd17ddefcd2a23cbc
Taeyeon <lty0314@gmail.com>
2024-08-23 12:19:20 +0900

    feat: alert SweetAlert2 적용 refactor : context API로 리팩터링

---
+++ src/context/PokemonProvider.jsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useState } from "react";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+
+const PokemonContext = createContext();
+
+export function usePokemon() {
+  return useContext(PokemonContext);
+}
+
+export function PokemonProvider({ children }) {
+  const [selectedPokemon, setSelectedPokemon] = useState([]);
+
+  const addPokemon = (pokemon) => {
+    if (selectedPokemon.length >= 6) {
+      //   alert("최대 6개의 포켓몬만 선택할 수 있습니다.");
+      Swal.fire({
+        html: '<div style="font-size: 25px;">최대 6개의 포켓몬만 선택할 수 있습니다.</div>',
+      });
+      return;
+    }
+    if (!selectedPokemon.some((p) => p.id === pokemon.id)) {
+      setSelectedPokemon((prevSelected) => [...prevSelected, pokemon]);
+    }
+  };
+
+  const removePokemon = (pokemon) => {
+    setSelectedPokemon((prevSelected) =>
+      prevSelected.filter((p) => p.id !== pokemon.id)
+    );
+  };
+
+  return (
+    <PokemonContext.Provider
+      value={{ selectedPokemon, addPokemon, removePokemon }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  );
+}
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import { styled } from "styled-components";
 import PokemonList from "../components/PokemonList";
 import MOCK_DATA from "../mock";
 import Dashboard from "../components/Dashboard";
+import { usePokemon } from "../context/PokemonProvider";
 
 const StDexMain = styled.main`
   background-color: #ffc6c6d4;
@@ -18,23 +18,7 @@ const StDexMain = styled.main`
 `;
 
 function Dex() {
-  const [selectedPokemon, setSelectedPokemon] = useState([]);
-
-  const addPokemon = (pokemon) => {
-    if (selectedPokemon.length >= 6) {
-      alert("최대 6개의 포켓몬만 선택할 수 있습니다.");
-      return;
-    }
-    if (!selectedPokemon.some((p) => p.id === pokemon.id)) {
-      setSelectedPokemon((prevSelected) => [...prevSelected, pokemon]);
-    }
-  };
-
-  const removePokemon = (pokemon) => {
-    setSelectedPokemon((prevSelected) =>
-      prevSelected.filter((p) => p.id !== pokemon.id)
-    );
-  };
+  const { selectedPokemon, addPokemon, removePokemon } = usePokemon();
 
   return (
     <StDexMain>
