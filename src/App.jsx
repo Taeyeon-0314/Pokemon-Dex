1f659cc902375489c5f91bce5be1567dd3eb74ff
Taeyeon <lty0314@gmail.com>
2024-08-23 09:46:10 +0900

    refactor: PokemonDetail 컴포넌트를 pages/Detail.jsx로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dex from "./pages/Dex";
-import PokemonDetail from "./components/PokemonDetail";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         {/* 포켓몬도감과 디테일 페이지 추가 */}
         <Route path="/dex" element={<Dex />} />
-        <Route path="/pokemon-detail" element={<PokemonDetail />} />
+        <Route path="/pokemon-detail" element={<Detail />} />
       </Routes>
     </Router>
   );
